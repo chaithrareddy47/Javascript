99c4d31cb5c210af93061b1adca7e5bab6961ba4
chaithra reddy <142560987+chaithrareddy47@users.noreply.github.com>
2025-01-03 21:03:56 +0530

    Add files via upload

---
+++ 03-Datatypes/App.js
@@ -0,0 +1,36 @@
+let age = 10;
+const name = "chaithra "
+let x;
+let y = null;
+let isEmployed = true;
+let isAdult = false;
+
+console.log("type of age is " +  typeof age);
+console.log("type of name is " +  typeof name);
+console.log("type of x is " +  typeof x);
+console.log("type of y is " +  typeof y);
+console.log("type of isEmployed is " +  typeof isEmployed);
+console.log("type of adult is " +  typeof isAdult);
+
+
+if (typeof x === undefined) {
+    console.log('x is defined');
+    
+} else {
+    console.log('x is undefined');
+    
+}
+
+//Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+
+let isIsland = false;
+let language;
+
+let population = 1400;
+const country = "India";
+ 
+console.log(typeof isIsland); // boolean
+console.log(typeof population) // number
+console.log(typeof country);// string
+console.log(typeof language); // undefinded
+
