3f7cb2dda380e5a5e4e7960e3a93899a64b8feb8
chaithra reddy <142560987+chaithrareddy47@users.noreply.github.com>
2025-01-03 21:04:23 +0530

    Add files via upload

---
+++ 01-Hello-World/App.js
@@ -0,0 +1,24 @@
+console.log("I’m JavaScript!");
+
+// alert("I’m JavaScript!");
+
+// VARIABLES
+let name1 = "john";
+let admin; // declared but not assigmed any value
+// name1= admin here we are assigning the undefined to name1 so we get o/p as undefined insted og actaul value
+
+admin = name1;
+
+console.log(admin);
+
+alert(admin);
+
+
+// constants will not change 
+const ourPlanet = "earth";
+
+let currentWebsiteVisitor = "cahithra";
+
+
+const BIRTHDAY = "03.04.1998"; // Uppercase for hardcoded immutable constants
+let age = someCode(BIRTHDAY); // Lowercase and 'let' because age changes over time
