0c13c63025ea6ac803b3dffaff24d5edec37ad26
chaithra reddy <142560987+chaithrareddy47@users.noreply.github.com>
2025-01-03 20:54:59 +0530

    Add files via upload

---
+++ 02-Variables/App.js
@@ -0,0 +1,12 @@
+// variables
+
+// Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+const country = "India";
+
+const continent = "Asia";
+
+let population = 1400; // popu;ation in millions
+
+console.log(country + " " + continent + " " + population);
+
