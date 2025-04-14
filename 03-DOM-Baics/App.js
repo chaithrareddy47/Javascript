63308d81f73a99ad0e50bc5df4499a87535f3266
chaithra reddy <142560987+chaithrareddy47@users.noreply.github.com>
2025-04-14 21:24:09 +0530

    Update App.js

--- 03-DOM-Baics/App.js
+++ 03-DOM-Baics/App.js
@@ -3,4 +3,25 @@
 */
 // * selecting and manipulating dom
 
+/*
 console.log(document.querySelector('.message').textContent);
+
+
+// selelcting elemnts
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 30;
+console.log(document.querySelector('.guess').value);
+
+console.log(document.querySelector('.guess').value = 23);
+
+*/
+const inputVal = (document.querySelector(".number").textContent = 13);
+
+
+// * Event listeners
+
+const btn = document.querySelector(".check").addEventListener('click', function () {
+     console.log("Button clicked");
+     
+}) 
+console.log(btn);
