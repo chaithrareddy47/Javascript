1daa0cccff218d3bad04aa3549890ac30b664f0f
chaithra reddy <142560987+chaithrareddy47@users.noreply.github.com>
2025-04-25 21:29:59 +0530

    Update PractiseQ.js

--- 06-OOPS/PractiseQ.js
+++ 06-OOPS/PractiseQ.js
@@ -1,25 +1,18 @@
-class BankAccout{
-    constructor(balance = 0, accountHolder, accountNumber) {
-        this.accountHolder = accountHolder;
-        this.accountNumber = accountNumber;
-        this.balance = balance;
+class Teacher{
+    constructor(name, channel, likes=0) {
+        this.name = name;
+        this.channel = channel;
+        this.likes = likes
     }
 
-    deposit(amount) {
-      
-        if (amount > 0) {
-            this.balance += amount
-            c
-            
-        }
-    }
-
-    withDraw() {
-        
+    likesFunc() {
+        let vedioLikes = this.likes++
+        return `Welcome to ${this.channel} , Thank you for ${vedioLikes} likes`;
     }
 }
 
-const alexAccount = new BankAccout('123Abc', 'Alex', 600)
-
-console.log(alexAccount.deposit());
+const obj1 = new Teacher('pyush', 'roadside coder')
+obj1.likesFunc()
+obj1.likesFunc()
+console.log(obj1.likesFunc());
 
